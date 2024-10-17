9ec68f0af624d8095f4e175e5f6878d8b98d580c
Adrian Lee <alee.lcagency@gmail.com>
2024-10-18 08:16:36 +0900

    refactor: assignment seeder

--- seeder/assignment.go
+++ seeder/assignment.go
@@ -1,7 +1,6 @@
 package seeder
 
 import (
-	"database/sql"
 	"fmt"
 	"math/rand"
 	"time"
@@ -12,10 +11,31 @@ import (
 	"github.com/brianvoe/gofakeit/v7"
 )
 
-func AssignmentSeeder(db *sql.DB) {
+type assignmentSeeder struct {
+	assignmentRepo repository.AssignmentRepository
+	courseRepo     repository.CourseRepository
+	faker          faker
+	randomizer     randomizer
+}
+
+func NewAssignmentSeeder(
+	assignmentRepo repository.AssignmentRepository,
+	courseRepo repository.CourseRepository,
+	faker faker,
+	randomizer randomizer,
+) *assignmentSeeder {
+	return &assignmentSeeder{
+		assignmentRepo: assignmentRepo,
+		courseRepo:     courseRepo,
+		faker:          faker,
+		randomizer:     randomizer,
+	}
+}
+
+func (s *assignmentSeeder) Seed() {
 	minAssignmentCount := 3
-	courseRepository := repository.NewCourseRepository(db)
-	courseIDs := courseRepository.GetCourseIDs()
+
+	courseIDs := s.courseRepo.GetCourseIDs()
 	// increasing the ratio to approved vs false to 6:1
 	gradedOption := []bool{true, true, true, true, true, false}
 	randomTitles := []string{
@@ -49,8 +69,7 @@ func AssignmentSeeder(db *sql.DB) {
 			assignmentModelLinks = append(assignmentModelLinks, modelLink)
 		}
 	}
-	// TODO clean up repository struct creation (perhaps from main then pass along the seeding flow?)
-	assignmentRepo := repository.NewAssignmentRepository(db)
-	assignmentRepo.InsertMultipleAssignments(assignmentModelLinks)
+
+	s.assignmentRepo.InsertMultipleAssignments(assignmentModelLinks)
 	fmt.Println("Finish seeding Assignment")
 }
